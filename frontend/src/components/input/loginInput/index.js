a32297bba985e175ae298e099de9e362b6ce04d2
AliFaiizan <alifaizanahmad@gmail.com>
2022-09-10 00:59:02 +0500

    frontend/login

--- frontend/src/components/input/loginInput/index.js
+++ frontend/src/components/input/loginInput/index.js
@@ -1,15 +1,19 @@
 import { ErrorMessage, useField } from 'formik';
 import React from 'react'
 import './loginInputstyles.css'
-
+import {useMediaQuery} from 'react-responsive';
 
 export default function LoginInput({placeholder,bottom,...props}) {
-
+   const desktopView= useMediaQuery({
+    query:"(min-width:850px)"
+   })
+   
    const [field,meta] = useField(props)
   return (
     <div className="input-wrap">
-      {meta.touched&&meta.error&& !bottom &&
-      <Error field={field}/>}
+      {meta.touched && meta.error && !bottom && (
+        <Error field={field} desktopView={desktopView} />
+      )}
       <input
         className={meta.touched && meta.error ? "input-error-border" : ""}
         {...field}
@@ -17,28 +21,39 @@ export default function LoginInput({placeholder,bottom,...props}) {
         name={field.name}
         placeholder={placeholder}
       />
-      {meta.touched && meta.error && bottom && <Error field={field} bottom />}
+      {meta.touched && meta.error && bottom && (
+        <Error field={field} desktopView={desktopView} bottom />
+      )}
+
+      {/* icon */}
       {meta.touched && meta.error && (
-        <i className="error_icon" style={{ top: `${!bottom && "63%"}` }}></i>
+        <i
+          className="error_icon"
+          style={{ top: `${!bottom && !desktopView && '60%' } ` }}
+        ></i>
       )}
     </div>
   );
 } 
 
-const Error=({field,bottom}) => {
-    
+const Error=({field,bottom,desktopView}) => {
+    console.log(desktopView)
     if(bottom){
         return (
-          <div className="input-error">
-              <ErrorMessage name={field.name} />
-              <div className="error-arrow-bottom"></div>
+          <div className={desktopView ? "input-error-desktop" : "input-error"}>
+            <ErrorMessage name={field.name} />
+            <div
+              className={
+                desktopView ? "error-arrow-desktop" : "error-arrow-bottom"
+              }
+            ></div>
           </div>
-        )}else if(!bottom){
+        );}else if(!bottom){
             
             return (
-              <div className="input-error">
+              <div className={desktopView?"input-error-desktop":"input-error"}>
                 <ErrorMessage name={field.name} />
-                <div className="error-arrow-top"></div>
+                <div className={desktopView?"error-arrow-desktop":"error-arrow-top"}></div>
               </div>
             );
         }
